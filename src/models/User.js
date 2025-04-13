cd1dc082db838785248aee000d43e899748ae22e
Smaash7 <128426063+Smaash7@users.noreply.github.com>
2025-04-13 17:04:29 +0100

    LoginRoute

--- src/models/User.js
+++ src/models/User.js
@@ -21,7 +21,8 @@ const userSchema = new mongoose.Schema({
         type: String,
         default:""
     }
-});
+}, {timestamps: true}
+);
 
 //hash password before saving user to db
 userSchema.pre("save", async function(next) {
@@ -30,8 +31,13 @@ userSchema.pre("save", async function(next) {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 
-    next()
-})
+    next();
+});
+
+//compare passwords func
+userSchema.methods.comparePassword = async function (userPassword) {
+    return await bcrypt.compare(userPassword, this.password);  
+};
 
 
 const User = mongoose.model("User", userSchema);
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,12 @@
 import express from "express";
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
+
+const generateToken = (userId) => {
+    return jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: "15d"});
+}
 
 router.post("/register", async(req, res) => {
     try{
@@ -11,11 +16,11 @@ router.post("/register", async(req, res) => {
             return res.status(400).json({ message: "All fields are required"});
         }
 
-        if (password.length <6){
-            return res.status(400).json({ message: "Password should be at least 6 characters long"});
+        if (password.length <5){
+            return res.status(400).json({ message: "Password should be at least 5 characters long"});
         }
-        if(username.length < 3){
-            return res.status(400).json({ message: "Username should be at least 3 characters long"});
+        if(username.length < 2){
+            return res.status(400).json({ message: "Username should be at least 2 characters long"});
         }
 
         const existingEmail = await User.findOne({ email });
@@ -40,13 +45,56 @@ router.post("/register", async(req, res) => {
 
         await user.save();
 
+        const token = generateToken(user._id);
+
+        res.status(201).json({
+            token,
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+                profileImage: user.profileImage
+            }
+        })
+
     } catch (error){
+        console.log("Error in register route", error);
+        res.status(500).json({ message: "Internal server error"});
 
     }
 });
 
-router.post("/login", async(req, res) => {
-    res.send("login");
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) return res.status(400).json({ message: "All fields are required" });
+
+        // Check if user exists
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ message: "Credentials not valid" });
+
+        // Check if password is correct
+        const isPasswordCorrect = await user.comparePassword(password);
+        if (!isPasswordCorrect) return res.status(400).json({ message: "Credentials not valid" });
+
+
+        const token = generateToken(user._id);
+        res.status(200).json({
+            token,
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+                profileImage: user.profileImage
+                }
+                }
+                );
+
+    } catch (error) {
+        console.log("Error in login route", error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
 });
 
 export default router;
