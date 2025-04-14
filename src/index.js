2eb680e7c972aac26e7828f59256db6d86ce3886
Smaash7 <128426063+Smaash7@users.noreply.github.com>
2025-04-14 02:36:58 +0100

    barberRoute

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 
 import authRoutes from "./routes/authRoutes.js";
+import barberRoutes from "./routes/barberRoutes.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
@@ -9,7 +10,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use("/api/auth",authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/barbers", barberRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
