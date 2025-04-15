870cd18778a9aa3bcc44f4b27a6119291e9896d8
Smaash7 <128426063+Smaash7@users.noreply.github.com>
2025-04-15 12:26:33 +0100

    image

--- mobile/app/(auth)/index.jsx
+++ mobile/app/(auth)/index.jsx
@@ -1,9 +1,27 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import React, { useState } from "react";
+import styles from "../../assets/styles/login.styles"
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = () => {}
+
+
   return (
-    <View>
-      <Text>Login Screen</Text>
+    <View style={styles.container}>
+       {/*IMAGE*/}
+       <View style={styles.topIllustration}>
+        <Image
+        source={require("../../assets/images/i2.png")}
+        style={styles.illustrationImage}
+        resizeMode="contain"
+        />
+       </View>
+      
     </View>
   );
 }
