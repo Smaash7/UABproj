0837ce5483dee241c043b996dc2adefb332a5b6d
Smaash7 <128426063+Smaash7@users.noreply.github.com>
2025-04-19 15:27:30 +0100

    CreatePage (50%)

--- mobile/app/(tabs)/create.jsx
+++ mobile/app/(tabs)/create.jsx
@@ -1,10 +1,193 @@
-import { View, Text } from 'react-native';
+import { View, Text, KeyboardAvoidingView, Platform, ScrollView, TextInput, TouchableOpacity, Alert, Image, ActivityIndicator,} from 'react-native';
 import React from 'react';
+import {useState } from 'react';
+import { useRouter } from "expo-router";
+import styles from "../../assets/styles/create.styles";
+import { Ionicons } from '@expo/vector-icons';
+import COLORS from '../../constants/colors';
+import { useAuthStore} from '../../store/authStore';
+
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
 export default function Create() {
+  const [title, setTitle] = useState("");
+  const [caption, setCaption] = useState("");
+  const [rating, setRating] = useState(3);
+  const [image, setImage] = useState(null); // to display the selected  image
+  const [imageBase64, setImageBase64] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+  const {token }= useAuthStore();
+
+
+  const pickImage = async () => {
+    try {
+      // request permission if needed
+      if  (Platform.OS === "android") {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          Alert.alert('Permission to access camera roll is required for this feature to work');
+          return;
+          }
+        }
+
+        //launch image library
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: "images",
+        allowsEditing: true,
+        aspect: [4,3],
+        quality: 0.5,
+        base64: true,
+        })
+      
+      if (!result.canceled) {
+       
+        setImage(result.assets[0].uri);
+
+        // if base64 is provided, use it
+        if (result.assets[0].base64) {
+          setImageBase64(result.assets[0].base64);
+          } else {
+            // otherwise, convert to base64
+            const base64 = await convertToBase64(result.assets[0].uri, {
+              endcoding: FileSystem.EndcodingType.Base64,
+            });
+            setImageBase64(base64);
+          }
+      } 
+    } catch (error) {
+      console.error("Error picking image", error);
+      Alert.alert ('Error picking image', error.message); 
+    }
+  };
+
+  const handleSubmit = async () => {
+    if (!title || !caption || !imageBase64 || !rating) {
+      Alert.alert('Please fill in all fields');
+      return;
+    }
+
+    try {
+      setLoading(true);
+
+      //const  token = await AsyncStorage.getItem('token');
+      
+
+    } catch (error) {
+      
+    }
+  };
+
+  const renderRatingPicker = () => {
+
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      stars.push(
+          <TouchableOpacity key={i} onPress={() => setRating(i)} style={styles.starButton}>
+            <Ionicons
+              name={i <= rating ? "star" : "star-outline"}
+              size={32}
+              color={i <= rating ? "#f4b400" : COLORS.textSecondary}
+            />
+            </TouchableOpacity>
+            );
+            }
+            return <View style={styles.ratingContainer}>{stars}</View>
+  };
+
   return (
-    <View>
-      <Text>create tab</Text>
-    </View>
+    <KeyboardAvoidingView
+    style={{ flex: 1}}
+    behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView contentContainerStyle={styles.container} style={styles.scrollViewStyle}>
+        <View style={styles.card}>
+          {/*Header*/}
+          <View style={styles.header}>
+            <Text style={styles.title}>Add Barber Recommendation</Text>
+            <Text style={styles.subtitle}>Share your favorite barber with us!</Text>
+          </View>
+
+          <View style={styles.form}>
+            {/*Barber*/}
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Barber Name</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                name="storefront-outline"
+                size={20}
+                color={COLORS.placeholderText}
+                style={styles.inputIcon}
+                />
+                <TextInput
+                style={styles.input}
+                placeholder="Barber Name"
+                placeholderText={COLORS.placeholderText}
+                value={title}
+                onChangeText={setTitle}
+                />
+              </View>
+            </View>
+          
+            {/*Rating*/}
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Your Rating</Text>
+              {renderRatingPicker()}
+
+            </View>
+
+            {/*IMAGE*/}
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Barber Image</Text>
+              <TouchableOpacity style={styles.imagePicker} onPress={pickImage}>
+                {image ? (
+                  <Image source={{ uri: image }} style={styles.previewImage} />
+                  ) : (
+                    <View style={styles.placeholderContainer}>
+                      <Ionicons name="image-outline" size={40} color={COLORS.textSecondary} />
+                      <Text style={styles.placeholderText}>Tap to select image</Text>
+                    </View>
+                )}
+
+
+              </TouchableOpacity>
+
+
+            </View>
+
+            {/*CAPTION*/}
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Caption</Text>
+              <TextInput
+              style={styles.textArea}
+              placeholder="Write here your Review"
+              placeholderTextColor={COLORS.placeholderText}
+              value={caption}
+              onChangeText={setCaption}
+              multiline
+              />
+            </View>
+
+            <TouchableOpacity style={styles.button} onPress={handleSubmit} disabled={loading}>
+              {loading ? (
+                <ActivityIndicator color={COLORS.white} size="large" />
+                ) : (
+                  <>
+                  <Ionicons
+                  name="cloud-upload-outline"
+                  size={20}
+                  color={COLORS.white}
+                  style={styles.buttonIcon}    
+                  />
+                  <Text style={styles.buttonText}>Submit Review</Text>
+                  </>   
+                )}
+                </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
